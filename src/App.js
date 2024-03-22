ca5d92ee7a4509128470903a0f94895a1ef8ebac
João vitor Silva Ramos <joao.silvaramos2013@hotmail.com>
2024-03-22 15:30:38 -0300

    Feat: criado a funcao para buscar a localizacao do usuario

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Container from "./components/Container"
 import Main from "./components/Main"
 import getTime from "./utils/getTime"
 import getData from "./utils/getData"
+import {getLocality} from "./utils/getLocality"
 import { useState, useEffect } from "react"
 
 function App() {
@@ -10,10 +11,11 @@ function App() {
   // Defina o estado inicial como um objeto vazio
   const [time, setTime] = useState({ hours: 0, minutes: 0, seconds: 0 });
   const [date, setData] = useState({ day: 0, month: 0, year: 0 });
+  const [userLocationLatitude, setUserLocationLatitude] = useState("");
+  const [userLocationLongitude, setUserLocationLongitude] = useState("");
 
   // Use useEffect para chamar getTime() quando o componente for montado
   useEffect(() => {
-
     setData(getData())
 
     const intervalId = setInterval(() => {
@@ -22,12 +24,26 @@ function App() {
 
     // Limpe o intervalo quando o componente for desmontado
     return () => clearInterval(intervalId);
-  }, []); 
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const { latitude, longitude } = await getLocality();
+            setUserLocationLatitude(latitude);
+            setUserLocationLongitude(longitude);
+        } catch (error) {
+            console.error('Erro ao obter a localização:', error);
+        }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <Container>
       <Header hour={time.hours} minutes={time.minutes} seconds={time.seconds} />
-      <Main cityName="Itapecerica da serra" temperature="30" data={`${date.day} ${date.month}, ${date.year}`} humidity="99" visiblity="2" airPressure="22" wind="1"/>
+      <Main cityName={`${userLocationLatitude}, ${userLocationLongitude}`} temperature="30" data={`${date.day} ${date.month}, ${date.year}`} humidity="99" visiblity="2" airPressure="22" wind="1"/>
     </Container>
   );
 }
