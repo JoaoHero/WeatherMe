debd0bda62bf8adfa0d0bcf47cad4f970aa25c40
João vitor Silva Ramos <joao.silvaramos2013@hotmail.com>
2024-03-24 16:42:25 -0300

    Fix: Foi corrigido a logica da apliacacao para nao buscar os dados pelo frontend, integrado a um servidor propio onde a api ira retornando os dados a serem apresentado. Feat: Foi criado toda a logica e integracao com o beckend

--- src/utils/getLocality.js
+++ src/utils/getLocality.js
@@ -1,13 +1,18 @@
 function getLocality() {
     return new Promise((resolve, reject) => {
+        const defaultLocation = { latitude: -22.9068 , longitude: -43.1729 };
+
         navigator.geolocation.getCurrentPosition(
             async function (position) {
                 const latitude = position.coords.latitude;
                 const longitude = position.coords.longitude;
+
                 resolve({ latitude, longitude }); 
             },
             function (error) {
-                reject(error);
+                console.error('Erro ao obter localização:', error);
+                // Se ocorrer um erro, resolva a promessa com o valor padrão
+                resolve(defaultLocation);
             }
         );
     });
