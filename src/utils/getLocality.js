67b4f087fac482a880358a1716d3a171e916dc5d
João vitor Silva Ramos <joao.silvaramos2013@hotmail.com>
2024-03-24 22:56:05 -0300

    Feat: Foi criado a tratativa dos dados e das excecoes

--- src/utils/getLocality.js
+++ src/utils/getLocality.js
@@ -1,21 +1,25 @@
 function getLocality() {
     return new Promise((resolve, reject) => {
-        const defaultLocation = { latitude: -22.9068 , longitude: -43.1729 };
+        const defaultLocation = { latitude: -22.9068 , longitude: -43.1729 }
 
         navigator.geolocation.getCurrentPosition(
             async function (position) {
                 const latitude = position.coords.latitude;
                 const longitude = position.coords.longitude;
+                localStorage.setItem('latitude', latitude);
+                localStorage.setItem('longitude', longitude);
 
-                resolve({ latitude, longitude }); 
+                resolve({ latitude, longitude })
             },
             function (error) {
-                console.error('Erro ao obter localização:', error);
+                const { latitude, longitude } = defaultLocation;
+                localStorage.setItem('latitude', latitude);
+                localStorage.setItem('longitude', longitude);
                 // Se ocorrer um erro, resolva a promessa com o valor padrão
-                resolve(defaultLocation);
+                resolve(defaultLocation)
             }
-        );
-    });
+        )
+    })
 }
 
 export {
